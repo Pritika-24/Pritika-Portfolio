70ee584af855d0175f7b91369a11794966748206
Pritika-24 <114833469+Pritika-24@users.noreply.github.com>
2023-08-10 23:21:33 -0400

    added

--- components/Avatar.js
+++ components/Avatar.js
@@ -5,7 +5,7 @@ const Avatar = () => {
   return (
     <div className='hidden xl:flex xl:max-w-none'>
       <Image
-        src={'/My project.png'}
+        src={'/Pritikaface.png'}
         width={737}
         height={678}
         alt=''
